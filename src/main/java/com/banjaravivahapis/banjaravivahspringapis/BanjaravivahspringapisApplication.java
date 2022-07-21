d12c75cf25761e9df742e567cced00893e1ff186
swapnil rathod <rathodswapnil9@gmail.com>
2022-07-21 14:58:38 +0530

    first commit

---
+++ src/main/java/com/banjaravivahapis/banjaravivahspringapis/BanjaravivahspringapisApplication.java
@@ -0,0 +1,14 @@
+package com.banjaravivahapis.banjaravivahspringapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanjaravivahspringapisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanjaravivahspringapisApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/banjaravivahapis/banjaravivahspringapis/BanjaravivahspringapisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banjaravivahapis.banjaravivahspringapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanjaravivahspringapisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
